918a69e664175bf1cee6141bbbdd6349640c1ced
Himanshu <himanshuyadav9536w@gmail.com>
2025-08-27 18:01:50 +0530

    Refactor db cred through env

--- config/index.js
+++ config/index.js
@@ -1,16 +1,46 @@
 require('dotenv').config();
 
 const config = {
-    port: process.env.E_PORT,
-    host:process.env.HOST,
-    email_api_key: process.env.EMAIL_API_KEY,
-    cashfree_api_key: process.env.CASHFREE_API_KEY,
-    cashfree_secret_key: process.env.CASHFREE_SECRET_KEY,
-    jwt_key: process.env.JWT_KEY,
-    IAM_ACCESS_KEY: process.env.IAM_ACCESS_KEY,
-    IAM_SECRET_KEY: process.env.IAM_SECRET_KEY,
-    BUCKET_NAME: process.env.BUCKET_NAME,
-    AWS_REGION: process.env.AWS_REGION,
+  port: process.env.E_PORT,
+  host: process.env.HOST,
+  email_api_key: process.env.EMAIL_API_KEY,
+  cashfree_api_key: process.env.CASHFREE_API_KEY,
+  cashfree_secret_key: process.env.CASHFREE_SECRET_KEY,
+  jwt_key: process.env.JWT_KEY,
+  IAM_ACCESS_KEY: process.env.IAM_ACCESS_KEY,
+  IAM_SECRET_KEY: process.env.IAM_SECRET_KEY,
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  AWS_REGION: process.env.AWS_REGION,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_DIALECT: process.env.DB_DIALECT
 }
 
 module.exports = config;
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": "Server123",
+//     "database": "expensesapp",
+//     "host": "localhost",
+//     "dialect": "mysql",
+//     "logging":false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
--- models/index.js
+++ models/index.js
@@ -4,17 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config');
 
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+let sequelize = new Sequelize(config.DB_NAME, config.DB_USERNAME, config.DB_PASSWORD, { host: config.DB_HOST, dialect: config.DB_DIALECT, logging:false });
+
 
 // Load and register all models
 fs.readdirSync(__dirname)
